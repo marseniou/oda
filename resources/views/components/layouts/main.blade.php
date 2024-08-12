267f335eb5f9c68926c7c2c3e57b176d19928ddd
Marios <marseniou@gmail.com>
2024-08-12 21:58:07 +0300

    added tailwind and daisyui

---
+++ resources/views/components/layouts/main.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="sunset">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>{{$title ?? env('APP_NAME')}}</title>
+    @vite('resources/js/app.js')
+</head>
+
+<body class="bg-base-100">
+    <x-navigation></x-navigation>
+    {{$slot}}
+</body>
+
+</html>
